a20525db96912b51f48da20819ac36a19f57c9b5
William McDonnell <william.mcdonnell.2016@mumail.ie>
2021-02-03 20:09:35 +0000

    Updated classes: Ball, Player, Frame

--- Classes/Frame.py
+++ Classes/Frame.py
@@ -2,6 +2,7 @@ import cv2
 from . import Ball
 from . import Player
 
+
 class Frame:
 
     def __init__(self, circles):
@@ -9,15 +10,15 @@ class Frame:
         p1_colour = 'purple'
         p2_colour = 'orange'
 
-        self.player1 = Player(p1_colour)
-        self.player2 = Player(p2_colour)
+        self.player1 = Player.Player(p1_colour)
+        self.player2 = Player.Player(p2_colour)
+        self.balls = []
 
         if circles is not None:
             i = 0
             for (x, y, r, colour, ball_id) in circles:
                 loc = (x, y)
-                self.balls[i] = Ball(circles.colour, loc, ball_id)
-
+                self.balls[i] = Ball.Ball(circles.colour, loc, ball_id)
 
     # def obtain_scores(self):
         # add code to track ball
